7d2141d3c95eafd8f4ef6b3149fef4f5c2de918b
bashar khdr <bashar.khdr.DS.92@hotmail.com>
2022-05-02 19:00:52 +0200

    adding attribution

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import logo from './images/logo.svg';
 import './App.css';
 import TipCalculator from './components/TipCalculator';
+import Appendix from './components/Appendix/Appendix';
 
 const App = () => {
   return (
     <div className="wrapper">
       <img src={logo} className="logo" alt="splitter" />
       <TipCalculator />
+      <Appendix />
     </div>
   );
 };
---
+++ src/components/Appendix/Appendix.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Appendix.css';
+
+const Appendix = () => {
+  return (
+    <div class="attribution">
+      Challenge by{' '}
+      <a
+        href="https://www.frontendmentor.io?ref=challenge"
+        rel={'noreferrer'}
+        target="_blank"
+      >
+        Frontend Mentor
+      </a>
+      . Coded by <button>Bashar Khdr</button>.
+    </div>
+  );
+};
+
+export default Appendix;
